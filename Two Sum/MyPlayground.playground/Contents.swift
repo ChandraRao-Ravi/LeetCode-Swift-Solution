b2354ec33dd250d7246fc8372e10d8788135c564
Chandra Rao <30439006+ChandraRao-Ravi@users.noreply.github.com>
2026-04-24 16:23:36 +0800

    Clean up Two Sum Problem Solution
    Clean up Two Sum Problem Solution

--- Two Sum/MyPlayground.playground/Contents.swift
+++ Two Sum/MyPlayground.playground/Contents.swift
@@ -1,72 +1,15 @@
 import Foundation
 
-public class ListNode {
-    public var val: Int
-    public var next: ListNode?
-    public init() { self.val = 0; self.next = nil; }
-    public init(_ val: Int) { self.val = val; self.next = nil; }
-    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
-}
-
-class Solution {
-    func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        var firstElement = ListNode(0)
-        var currentElement = firstElement
-        var carryForward = 0
-        var listNode1 = l1, listNode2 = l2
-        
-        while listNode1 != nil || listNode2 != nil || carryForward != 0 {
-            let v1 = listNode1?.val ?? 0
-            let v2 = listNode2?.val ?? 0
-            let sum = v1 + v2 + carryForward
-            carryForward = sum / 10
-            currentElement.next = ListNode(sum % 10)
-            currentElement = currentElement.next!
-            listNode1 = listNode1?.next
-            listNode2 = listNode2?.next
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var result: [Int: Int] = [:]
+    for (index, number) in nums.enumerated() {
+        let difference = target - number
+        if let j = result[difference] {
+            return [j, index]
         }
-        return firstElement.next
+        result[number] = index
     }
+    return []
 }
 
-// ##For Running in Playground
-
-// Helper to create list from array (reversed)
-func createList(from array: [Int]) -> ListNode? {
-    var head: ListNode?
-    var tail: ListNode?
-    for val in array {
-        let node = ListNode(val)
-        if head == nil {
-            head = node
-            tail = node
-        } else {
-            tail?.next = node
-            tail = node
-        }
-    }
-    return head
-}
-
-// Helper to print list
-func printList(_ head: ListNode?) {
-    var current = head
-    var values: [Int] = []
-    while current != nil {
-        values.append(current!.val)
-        current = current?.next
-    }
-    print("Result: \(values)")
-}
-
-// Test
-let l1Array = [2,4,3]  // 342
-let l2Array = [5,6,4]  // 465
-let l1 = createList(from: l1Array)
-let l2 = createList(from: l2Array)
-
-let solution = Solution()
-let result = solution.addTwoNumbers(l1, l2)
-
-printList(result)
-
+print(twoSum([7,8,9,2], 9))
